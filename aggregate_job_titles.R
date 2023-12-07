52077450ce91c3907f2385f4cd5980b9032178ae
Nguyen Tran <ntran0429@gmail.com>
2023-12-07 03:05:12 -0600

    updated EDA

--- aggregate_job_titles.R
+++ aggregate_job_titles.R
@@ -17,13 +17,15 @@ map_job_to_group <- function(job_title) {
   # Technical
   if (grepl("engineer", job_title) | grepl("programmer", job_title) | 
       grepl("analyst", job_title) | grepl("developer", job_title) | 
-      grepl("technician", job_title)) {
+      grepl("technician", job_title) | grepl("specialist", job_title) |
+      grepl("database administrator", job_title) | grepl("technical architect", job_title)) {
     return("Technical")
   }
   
   # Administrative
   if (grepl("assistant", job_title) | grepl("coordinator", job_title) | 
-      grepl("clerk", job_title) | grepl("secretary", job_title)) {
+      grepl("clerk", job_title) | grepl("secretary", job_title) |
+      grepl("representative", job_title) | grepl("recruiter", job_title)) {
     return("Administrative")
   }
   
@@ -31,8 +33,16 @@ map_job_to_group <- function(job_title) {
   if (grepl("biostatistician", job_title) | grepl("statistician", job_title) | 
       grepl("accountant", job_title) | grepl("financial analyst", job_title) |
       grepl("budget/accounting analyst", job_title) | grepl("actuary", job_title) |
-      grepl("compensation analyst", job_title) | grepl("mathematician", job_title)) {
+      grepl("compensation analyst", job_title) | grepl("mathematician", job_title) |
+      grepl("scientist", job_title)) {
     return("Quantitative")
+  }
+  
+  # Healthcare
+  if (grepl("nurse", job_title) | grepl("therapist", job_title) | 
+      grepl("health", job_title) | grepl("food", job_title) |
+      grepl("pathologist", job_title) | grepl("pharmacist", job_title)) {
+    return("Healthcare")
   }
   
   # Other
@@ -42,7 +52,7 @@ map_job_to_group <- function(job_title) {
 # Apply the mapping function to create a new column 'job_group'
 CD_CA_T$job_type <- sapply(CD_CA_T$job_title, map_job_to_group)
 final_dataset$job_type <- sapply(final_dataset$job_title, map_job_to_group)
-
+NewCustomerList$job_type <- sapply(NewCustomerList$job_title, map_job_to_group)
 
 # table(sapply(CustomerDemographic$job_title, map_job_to_group))
 
